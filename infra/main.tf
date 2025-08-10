c65ba137f0de02fba51a806abef247f67c10a855
Gkemhcs <gudikotieswarmani@gmail.com>
2025-08-10 17:58:58 +0530

    changed to gcp bucket for state

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,12 @@
 # Configure Terraform and providers
 terraform {
   required_version = ">= 1.0"
+  
+  backend "gcs" {
+    bucket = "kavach-terraform-state"
+    prefix = "terraform/state"
+  }
+  
   required_providers {
     google = {
       source  = "hashicorp/google"
